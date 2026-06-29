8d28b466de1ce541e514783aaa3453d724b8af9b
morangabonheur7-hue <morangabonheur7@gmail.com>
2026-06-29 20:52:01 +0100

    Update ZELIA_.py

--- ZELIA_.py
+++ ZELIA_.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="ZELIA GLOBAL", page_icon="🚀", layout="wide")
 
 # Modification ici : On s'assure d'avoir l'URL officielle chiffrée
 SUPABASE_URL = "https://qjfipgzuwkprfowgbimt.supabase.co"
-SUPABASE_KEY = os.environ.get("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqZmlwZ3p1w2twcmZvd2diaW10Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MDc2MDI0OSwiZXhwIjoyMDk2MzM2MjQ5fQ.zkDmslMSHuPtS2mJgC4qwWca5cq8IZUQMz6p6ecpTNA")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqZmlwZ3p1d2twcmZvd2diaW10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3NjAyNDksImV4cCI6MjA5NjMzNjI0OX0.rA17-omiRtXuECi0b7RW8wNe583Qa8swoV1HrgcQ9wM")
 RESEND_API_KEY = os.environ.get("RESEND_API_KEY", "re_7fidYWed_3hLMv1XeTBQ3urCAr9SQoHCz")
 
 if "authentifie" not in st.session_state: st.session_state.authentifie = False
